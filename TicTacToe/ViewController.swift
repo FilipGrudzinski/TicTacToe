f2b4d24c141b72442d58bc31f0d5cde3dc21c9fa
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-06-17 17:57:42 +0200

    TicTacToe + winner + some issues

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // player 1 is crosses , player 2 is noughts
+    
+    
+    @IBOutlet weak var board: UIImageView!
+    
     var player = 1
     
     var activeGame = true
@@ -19,7 +21,7 @@ class ViewController: UIViewController {
     
     let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
-    @IBAction func buttonPressed(_ sender: AnyObject) {
+    @IBAction func button(_ sender: AnyObject) {
         
         let activePosition = sender.tag - 1
         
@@ -28,43 +30,96 @@ class ViewController: UIViewController {
             gameState[activePosition] = player
             
             if player == 1 {
-            
+                label.text = ""
                 sender.setImage(UIImage(named: "cross.png"), for: [])
                 player = 2
-            
+                
             } else {
-            
+                
                 sender.setImage(UIImage(named: "nought.png"), for: [])
                 player = 1
-            
+                
             }
-            
+        
             for combination in winningCombinations {
                 
                 if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
                     
                     activeGame = false
-                    print(gameState[combination[0]])
+
+                    self.label.isHidden = false
+                    self.againButton.isHidden = false
+                    
+                if gameState[combination[0]] == 1 {
+                        
+                        label.text =  "Player crosses has won!! "
+                        
+                } else  {
+                    
+                        label.text =  "Player noughts has won!! "
                     
                 }
-                
-                
-            }
-            
-       // sender.setImage(UIImage(named: "cross.png"), for: [])
-        
-        //print(sender.tag)
-        
-    
-            
+                    
+                    UIView.animate(withDuration: 1, animations: {
+                        
+                        self.againButton.center = CGPoint(x: self.againButton.center.x, y: self.againButton.center.y + 100)
+                        self.label.center = CGPoint(x: self.label.center.x + 500, y: self.label.center.y)
+                        
+                    })
+                    
+                }
+
+           }
             
         }
         
     }
     
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var againButton: UIButton!
+    
+    @IBAction func againButtonPressed(_ sender: Any) {
+        
+        player = 1
+        
+        activeGame = true
+        
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        label.text = "Crosses Starting"
+        
+        againButton.isHidden = true
+        
+        for i in 1..<10 {
+            
+            if let button = view.viewWithTag(i) as? UIButton {
+                
+                button.setImage(nil, for: [])
+                
+            }
+            
+            board.alpha = 0
+            
+            UIView.animate(withDuration: 1, animations: {
+
+                   self.board.alpha = 1
+                
+            })
+        
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.isHidden = true
+        againButton.isHidden = true
+        self.label.center = CGPoint(x: self.label.center.x - 500, y: self.label.center.y)
+        self.againButton.center = CGPoint(x: self.againButton.center.x, y: self.againButton.center.y - 500)
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
