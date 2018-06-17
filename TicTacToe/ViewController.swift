c7b01fe9cdaae82ff2f247bcb8c165eb0a6e8cf5
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-06-17 12:52:45 +0200

    full playable game

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // player 1 is crosses , player 2 is noughts
+    var player = 1
+    
+    var activeGame = true
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        
+        let activePosition = sender.tag - 1
+        
+        if gameState[activePosition] == 0 && activeGame {
+            
+            gameState[activePosition] = player
+            
+            if player == 1 {
+            
+                sender.setImage(UIImage(named: "cross.png"), for: [])
+                player = 2
+            
+            } else {
+            
+                sender.setImage(UIImage(named: "nought.png"), for: [])
+                player = 1
+            
+            }
+            
+            for combination in winningCombinations {
+                
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+                    
+                    activeGame = false
+                    print(gameState[combination[0]])
+                    
+                }
+                
+                
+            }
+            
+       // sender.setImage(UIImage(named: "cross.png"), for: [])
+        
+        //print(sender.tag)
+        
+    
+            
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
